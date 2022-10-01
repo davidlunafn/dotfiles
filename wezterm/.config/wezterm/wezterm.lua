2c6fa0f2ea9d531e73bf28a01f6d9294d42c5c4a
David Luna <davidluna.fn@gmail.com>
2022-09-30 21:20:08 -0500

    TODO: scroll wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,7 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
+
+
 
 wezterm.on("update-right-status", function(window)
 	window:set_right_status(window:active_workspace())
@@ -12,17 +15,23 @@ end
 
 return {
 	-- General configuration
+    enable_wayland = true,
 	audible_bell = "Disabled",
 	window_close_confirmation = "NeverPrompt",
 	use_fancy_tab_bar = false,
 	enable_tab_bar = false,
-
-	window_padding = {
-		left = 2,
-		right = 2,
-		top = 2,
-		bottom = 2,
-	},
+    enable_scroll_bar = true,
+    min_scroll_bar_height = '2cell',
+    scrollback_lines = 3500,
+    colors = {
+    scrollbar_thumb = 'white',
+    },
+	--window_padding = {
+	--	left = 2,
+	--	right = 2,
+    --	top = 2,
+	--	bottom = 2,
+	--},
 
 	-- Font and color scheme
 	font = font_with_fallback("JetBrainsMono Nerd Font"),
@@ -57,109 +66,15 @@ return {
 
 	-- leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
 	disable_default_key_bindings = true,
+    mouse_bindings = {
+        {event={Up={streak=1, button="Right"}}, mods="NONE", action=wezterm.action{PasteFrom="PrimarySelection"}},
+    },
 	keys = {
-		{ key = "=", mods = "SUPER", action = wezterm.action.IncreaseFontSize },
+		{ key = "+", mods = "SUPER", action = wezterm.action.IncreaseFontSize },
 		{ key = "-", mods = "SUPER", action = wezterm.action.DecreaseFontSize },
-		{ key = "c", mods = "CTRL|SHIFT", action = wezterm.action.Copy },
-		{ key = "v", mods = "CTRL|SHIFT", action = wezterm.action.Paste },
-		-- {
-		-- 	key = "m",
-		-- 	mods = "LEADER",
-		-- 	action = wezterm.action.QuickSelectArgs({
-		-- 		label = "open url",
-		-- 		patterns = {
-		-- 			"https?://\\S+",
-		-- 		},
-		-- 		action = wezterm.action_callback(function(window, pane)
-		-- 			local url = window:get_selection_text_for_pane(pane)
-		-- 			wezterm.log_info("opening: " .. url)
-		-- 			wezterm.open_with(url)
-		-- 		end),
-		-- 	}),
-		-- },
-		-- {
-		-- 	key = "9",
-		-- 	mods = "ALT",
-		-- 	action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
-		-- },
-		--{ key = "n", mods = "LEADER|CTRL", action = act.SwitchWorkspaceRelative(1) },
-		--{ key = "p", mods = "LEADER|CTRL", action = act.SwitchWorkspaceRelative(-1) },
-		--{
-		--	key = "u",
-		--	mods = "LEADER|CTRL",
-		--	action = wezterm.action_callback(function(win, pane)
-
-		---- alternatives Ideas
-		---- workspaces are cool I like it, but the problem is when working with multiple projects
-		---- I dont like not being able to se the workspace name
-
-		--		-- here what I want have a project set or look for it
-		--		-- so need to interact with workspaces
-		--		--
-		--		-- so need a child process that runs fzf base on the list and return the name and directory
-		--		-- return as json {"name": "some name", "directory": "abs path", "group": "group"}
-		--		-- not sure what to do do with group yet but better to have it
-		----
-		---- launch menu does not provide a workspace
-		---- other option for a new workspace with a command that open
-		---- problem does not spawn a terminal or nothing visible
-		--		-- local success, stdout, stderr = wezterm.run_child_process { 'bash', '/home/alpha/.cargo/bin/tshort' }
-		---- main problem user input thing
-
-		---- if success then
-		----     win:perform_action(act.SwitchToWorkspace({
-		----         name = "some test",
-		----         spawn = {
-		----             cwd = "/home/alpha/.dotfiles",
-		----             args = { "nvim" },
-		----         },
-		----     }), pane)
-		---- end
-		--		-- act.SwitchToWorkspace {
-		--		-- name = 'name',
-		--		-- spawn = {
-		--		--   cwd = 'path of the project',
-		--		--   args = { 'nvim' },
-		--		-- },
-		--		-- },
-		--		-- very important this how to sync with other function perform action from window
-		--		-- win:perform_action(act.SwitchToWorkspace({
-		--		-- 	name = "some test",
-		--		-- 	spawn = {
-		--		-- 		cwd = "/home/adalessa/code/php/carin-gateway-service/",
-		--		-- 		args = { "nvim" },
-		--		-- 	},
-		--		-- }), pane)
-		--	end),
-		--},
-		-- { key = "n", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
-		-- { key = "\\", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-		-- { key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-
-		-- { key = "a", mods = "LEADER|CTRL", action = act.ActivateLastTab },
-		-- { key = "1", mods = "LEADER", action = act.ActivateTab(0) },
-		-- { key = "2", mods = "LEADER", action = act.ActivateTab(1) },
-		-- { key = "3", mods = "LEADER", action = act.ActivateTab(2) },
-
-		-- {
-		-- 	key = "h",
-		-- 	mods = "LEADER",
-		-- 	action = act.ActivatePaneDirection("Left"),
-		-- },
-		-- {
-		-- 	key = "l",
-		-- 	mods = "LEADER",
-		-- 	action = act.ActivatePaneDirection("Right"),
-		-- },
-		-- {
-		-- 	key = "k",
-		-- 	mods = "LEADER",
-		-- 	action = act.ActivatePaneDirection("Up"),
-		-- },
-		-- {
-		-- 	key = "j",
-		-- 	mods = "LEADER",
-		-- 	action = act.ActivatePaneDirection("Down"),
-		-- },
-	},
+		{ key = "c", mods = "CTRL", action = wezterm.action.Copy },
+		{ key = "v", mods = "CTRL", action = wezterm.action.Paste },
+        { key = 'h', mods = 'SHIFT', action = act.ScrollByLine(-1) },
+        { key = 'l', mods = 'SHIFT', action = act.ScrollByLine(1) },
+    },
 }
