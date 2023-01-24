56c1d82d117b695a83ec21f7224933920589ca1e
David Luna <davidluna.fn@gmail.com>
2023-01-23 22:24:48 -0500

    delete files

--- nvim/.config/nvim/lua/plugins/jukit.lua
+++ nvim/.config/nvim/lua/plugins/jukit.lua
@@ -1,6 +1,6 @@
 vim.g.jukit_mpl_style = '~/.local/share/nvim/lazy/vim-jukit/helpers/matplotlib-backend-kitty/backend.mplstyle'
-vim.g.jukit_terminal = 'kitty'
-vim.g.jukit_inline_plotting = 1
+vim.g.jukit_terminal = ''
+vim.g.jukit_inline_plotting = 0
 
 vim.g.jukit_output_new_os_window = 0
 vim.g.jukit_outhist_new_os_window = 0
@@ -8,5 +8,8 @@ vim.g.jukit_outhist_new_os_window = 0
 return {
     'luk400/vim-jukit',
     lazy = true,
-    keys = {{"<leader>np",':call jukit#convert#notebook_convert("jupyter-notebook")<cr>'}}
+    keys = {{"<leader>nn",':call jukit#convert#notebook_convert("jupyter-notebook")<cr>'},
+            {"<leader>nh",':call jukit#splits#output_and_history()<cr>'},
+            {"<leader>n",':call jukit#send#section(0)<cr>'},
+            {"<leader>no",':call jukit#splits#output()<cr>'}}
 }
