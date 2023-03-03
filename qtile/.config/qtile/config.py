8698357fbd237cc8d0bb16a0a65563d79285edd1
David Luna <davidluna.fn@gmail.com>
2023-03-02 22:34:37 -0500

    update bar

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -152,25 +152,15 @@ screens = [
     Screen(
         top=bar.Bar(
             [
-                #widget.CurrentLayout(),
-                widget.GroupBox(),
-                #widget.Prompt(),
-                #widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                #widget.TextBox("default config", name="default"),
-                #widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-                # widget.StatusNotifier(),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
-                widget.Spacer(length=20),
-                widget.Systray(),
+                widget.GroupBox(hide_unused=True),
+                widget.Spacer(),
+                widget.Clock(format="%I:%M %p"),
+                widget.Spacer(),
+                widget.QuickExit(default_text='󱄊', countdown_format='({})'),
+                widget.Spacer(length=10),
                 widget.Wallpaper(directory="/home/david/Imágenes/wallpapers",label=""),
+                widget.Systray(),
+                widget.Spacer(length=10),
             ],
             40,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
@@ -181,29 +171,15 @@ screens = [
     Screen(
         top=bar.Bar(
             [
-                #widget.CurrentLayout(),
                 widget.GroupBox(hide_unused=True),
                 widget.Spacer(),
-                #widget.Prompt(),
-                #widget.WindowName(),
-                #widget.Chord(
-                #    chords_colors={
-                #        "launch": ("#ff0000", "#ffffff"),
-                #    },
-                #    name_transform=lambda name: name.upper(),
-                #),
-                #widget.TextBox("default config", name="default"),
-                #widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-                # widget.StatusNotifier(),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+                widget.Clock(format="%I:%M %p"),
+                widget.Spacer(),
                 widget.QuickExit(default_text='󱄊', countdown_format='({})'),
-                widget.Spacer(length=20),
+                widget.Spacer(length=10),
                 widget.Wallpaper(directory="/home/david/Imágenes/wallpapers",label=""),
             ],
             40,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
     ),
 ]
