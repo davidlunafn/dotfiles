67de5758aadb83fd6d2bbdb335af43b33ae94297
David Luna <davidluna.fn@gmail.com>
2023-03-10 23:07:02 -0500

    configure bar qtile

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -1,42 +1,16 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-
+from typing import List  # noqa: F401
 import os
-from screeninfo import get_monitors
-from libqtile import bar, layout, widget
-from libqtile.config import Click, Drag, Group, Key, Match, Screen
+import subprocess
+from os import path
+
+from libqtile import bar, layout, widget, hook, qtile
+from libqtile.config import Click, Drag, Group, ScratchPad, DropDown, Key, Match, Screen
 from libqtile.lazy import lazy
-from libqtile.utils import guess_terminal
-
-
+from settings.path import qtile_path
+import colors
 
 mod = "mod4"
 terminal = "kitty"
-
 
 autostart = [
         "setxkbmap latam",
@@ -50,39 +24,26 @@ for x in autostart:
 
 
 keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "m", lazy.layout.next(), desc="Move window focus to other window"),
-    
 
     Key([mod], "space", lazy.spawn("rofi -show drun"), desc="Launch rofi"),
     Key([mod], "o", lazy.hide_show_bar("top"), desc="hide bar"),
     
-    
-
-
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
+    
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
     Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
+    
     Key(
         [mod, "shift"],
         "Return",
@@ -90,125 +51,315 @@ keys = [
         desc="Toggle between split and unsplit sides of stack",
     ),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
+    
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod, "shift"], "x", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+
+    #Control de Volumen
+    Key([], "XF86AudioLowerVolume", lazy.spawn("bash /home/david/.config/qtile/scripts/volume.sh down"), desc="Bajar Volumen"),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("bash /home/david/.config/qtile/scripts/volume.sh up"), desc="Subir Volumen"),
+    Key([], "XF86AudioMute", lazy.spawn("bash /home/david/.config/qtile/scripts/volume.sh mute"), desc="Mute Volumen"),
+
+    # Control de brillo
+    Key([], "XF86MonBrightnessUp", lazy.spawn("bash /home/david/.config/qtile/scripts/brightness-control.sh up"), desc="Subir Brillo"),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("bash /home/david/.config/qtile/scripts/brightness-control.sh down"), desc="Bajar Brillo"),
+
+    #Captura de pantalla
+    Key([], "print", lazy.spawn("scrot -s"), desc="Captura de pantalla"),
+    Key([mod], "Print", lazy.spawn("scrot"), desc="Subir Volumen"),
+
 ]
 
-groups = [Group(i) for i in "123456789"]
+# Create labels for groups and assign them a default layout.
+groups = []
 
+group_names = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "minus", "equal"]
+
+group_labels = ["", "", "", "", "", "", "", "", "ﭮ", "", "", "﨣"]
+# group_labels = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+
+group_layouts = ["monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall"]
+
+# Add group names, labels, and default layouts to the groups object.
+for i in range(len(group_names)):
+    groups.append(
+        Group(
+            name=group_names[i],
+            layout=group_layouts[i].lower(),
+            label=group_labels[i],
+        ))
+
+# Add group specific keybindings
 for i in groups:
-    keys.extend(
-        [
-            # mod1 + letter of group = switch to group
-           Key(
-                [mod],
-                i.name,
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
-            ),
-            # mod1 + shift + letter of group = switch to & move focused window to group
-            Key(
-                [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
-            ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
-        ]
-    )
+    keys.extend([
+        Key([mod], i.name, lazy.group[i.name].toscreen(),
+            desc="Mod + number to move to that group."),
+        Key(["mod1"], "Tab", lazy.screen.next_group(),
+            desc="Move to next group."),
+        Key(["mod1", "shift"], "Tab", lazy.screen.prev_group(),
+            desc="Move to previous group."),
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+            desc="Move focused window to new group."),
+    ])
+
+# Define scratchpads
+groups.append(ScratchPad("scratchpad", [
+    DropDown("term", "kitty --class=scratch", width=0.8, height=0.8, x=0.1, y=0.1, opacity=1),
+    DropDown("ranger", "kitty --class=ranger -e ranger", width=0.8, height=0.8, x=0.1, y=0.1, opacity=0.9),
+    DropDown("volume", "kitty --class=volume -e pulsemixer", width=0.8, height=0.8, x=0.1, y=0.1, opacity=0.9),
+    DropDown("mus", "kitty --class=mus -e ncmpcpp", width=0.8, height=0.8, x=0.1, y=0.1, opacity=0.9),
+    DropDown("news", "kitty --class=news -e newsboat", width=0.8, height=0.8, x=0.1, y=0.1, opacity=0.9),
+
+]))
+
+# Scratchpad keybindings
+keys.extend([
+    Key([mod], "n", lazy.group['scratchpad'].dropdown_toggle('term')),
+    Key([mod], "c", lazy.group['scratchpad'].dropdown_toggle('ranger')),
+    Key([mod], "v", lazy.group['scratchpad'].dropdown_toggle('volume')),
+    Key([mod], "m", lazy.group['scratchpad'].dropdown_toggle('mus')),
+    Key([mod], "b", lazy.group['scratchpad'].dropdown_toggle('news')),
+])
+
+colors, backgroundColor, foregroundColor, workspaceColor, chordColor = colors.custom()
+
+# Define layouts and layout themes
+layout_theme = {
+        "margin":5,
+        "border_width": 4,
+        "border_focus": colors[2],
+        "border_normal": backgroundColor
+    }
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=2, margin=4),
-    layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+    layout.MonadTall(**layout_theme),
+    layout.MonadWide(**layout_theme),
+    layout.Floating(**layout_theme),
+    layout.RatioTile(**layout_theme),
+    layout.Max(**layout_theme)
 ]
 
+# Mouse callback functions
+def launch_menu():
+    qtile.cmd_spawn("rofi -show drun -show-icons")
+
+
+# Define Widgets
 widget_defaults = dict(
     font="JetBrainsMono Nerd Font",
-    fontsize=20,
-    padding=4,
+    fontsize = 16,
+    padding = 2,
+    background=backgroundColor
 )
-extension_defaults = widget_defaults.copy()
 
+def init_widgets_list(monitor_num):
+    widgets_list = [
+        widget.GroupBox(
+            font="JetBrainsMono Nerd Font",
+            fontsize = 16,
+            margin_y = 2,
+            margin_x = 4,
+            padding_y = 6,
+            padding_x = 6,
+            borderwidth = 2,
+            disable_drag = True,
+            active = colors[4],
+            inactive = foregroundColor,
+            hide_unused = False,
+            rounded = False,
+            highlight_method = "line",
+            highlight_color = [backgroundColor, backgroundColor],
+            this_current_screen_border = colors[5],
+            this_screen_border = colors[7],
+            other_screen_border = colors[6],
+            other_current_screen_border = colors[6],
+            urgent_alert_method = "line",
+            urgent_border = colors[9],
+            urgent_text = colors[1],
+            foreground = foregroundColor,
+            background = backgroundColor,
+            use_mouse_wheel = False
+        ),
+        widget.TaskList(
+            icon_size = 0,
+            font = "JetBrainsMono Nerd Font",
+            foreground = colors[5],
+            background = colors[2],
+            borderwidth = 0,
+            border = colors[1],
+            margin = 0,
+            padding = 8,
+            highlight_method = "block",
+            title_width_method = "uniform",
+            urgent_alert_method = "border",
+            urgent_border = colors[1],
+            rounded = False,
+            txt_floating = "🗗 ",
+            txt_maximized = "🗖 ",
+            txt_minimized = "🗕 ",
+        ),
+        widget.Sep(
+            linewidth = 1,
+            padding = 10,
+            foreground = colors[5],
+            background = backgroundColor
+        ),
+        widget.OpenWeather(
+            app_key = "4cf3731a25d1d1f4e4a00207afd451a2",
+            cityid = "4997193",
+            format = '{icon} {main_temp}°',
+            metric = False,
+            font = "JetBrainsMono Nerd Font",
+            foreground = foregroundColor,
+        ),
+       widget.Sep(
+            linewidth = 0,
+            padding = 10
+        ),
+        widget.TextBox(
+            text = " ",
+            fontsize = 14,
+            font = "JetBrainsMono Nerd Font",
+            foreground = colors[7],
+        ),
+        widget.CPU(
+            font = "JetBrainsMono Nerd Font",
+            update_interval = 1.0,
+            format = '{load_percent}%',
+            foreground = foregroundColor,
+            padding = 5
+        ),
+        widget.Sep(
+            linewidth = 0,
+            padding = 10
+        ),
+        widget.TextBox(
+            text = "",
+            fontsize = 14,
+            font = "JetBrainsMono Nerd Font",
+            foreground = colors[3],
+        ),
+        widget.Memory(
+            font = "JetBrainsMonoNerdFont",
+            foreground = foregroundColor,
+            format = '{MemUsed: .0f}{mm} /{MemTotal: .0f}{mm}',
+            measure_mem='G',
+            padding = 5,
+        ),
+        widget.Sep(
+            linewidth = 0,
+            padding = 10
+        ),
+        widget.TextBox(
+            text = " ",
+            fontsize = 14,
+            font = "JetBrainsMono Nerd Font",
+            foreground = colors[10],
+        ),
+        widget.Clock(
+            format='%I:%M %p',
+            font = "JetBrainsMono Nerd Font",
+            padding = 10,
+            foreground = foregroundColor
+        ),
+        widget.Systray(
+            background = backgroundColor,
+            icon_size = 20,
+            padding = 4,
+        ),
+        widget.Sep(
+            linewidth = 1,
+            padding = 10,
+            foreground = colors[5],
+            background = backgroundColor
+        ),
+        widget.CurrentLayoutIcon(
+            scale = 0.5,
+            foreground = foregroundColor,
+            background = backgroundColor
+        ),
+        widget.Wallpaper(
+            directory="/home/david/Imágenes/wallpapers",
+            label=""),
+    ]
 
+    return widgets_list
+
+def init_secondary_widgets_list(monitor_num):
+    secondary_widgets_list = init_widgets_list(monitor_num)
+    del secondary_widgets_list[16:17]
+    return secondary_widgets_list
+
+widgets_list = init_widgets_list("1")
+secondary_widgets_list = init_secondary_widgets_list("2")
+secondary_widgets_list_2 = init_secondary_widgets_list("3")
+
+# Define 3 monitors
 screens = [
     Screen(
         top=bar.Bar(
-            [
-                widget.GroupBox(hide_unused=True),
-                widget.Spacer(),
-                widget.Clock(format="%I:%M %p"),
-                widget.Spacer(),
-                widget.QuickExit(default_text='󱄊', countdown_format='({})'),
-                widget.Spacer(length=10),
-                widget.Wallpaper(directory="/home/david/Imágenes/wallpapers",label=""),
-                widget.Systray(),
-                widget.Spacer(length=10),
-            ],
-            40,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+            widgets=widgets_list,
+            size=36,
+            background=backgroundColor,
+            margin=6, 
+            opacity=0.8
         ),
     ),
-
     Screen(
         top=bar.Bar(
-            [
-                widget.GroupBox(hide_unused=True),
-                widget.Spacer(),
-                widget.Clock(format="%I:%M %p"),
-                widget.Spacer(),
-                widget.QuickExit(default_text='󱄊', countdown_format='({})'),
-                widget.Spacer(length=10),
-                widget.Wallpaper(directory="/home/david/Imágenes/wallpapers",label=""),
-            ],
-            40,
+            widgets=secondary_widgets_list,
+            size=36,
+            background=backgroundColor,
+            margin=6, 
+            opacity=0.8
+        ),
+    ),
+    Screen(
+        top=bar.Bar(
+            widgets=secondary_widgets_list_2,
+            size=36,
+            background=backgroundColor,
+            margin=6, 
+            opacity=0.8
         ),
     ),
 ]
-
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front()),
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
 ]
 
+#Currently running Qtile in XFCE, so autostart script isn't necessary.  Uncomment if needed.
+#Startup applications
+#@hook.subscribe.startup_once
+#def autostart():
+#   home = os.path.expanduser('~/.config/qtile/scripts/autostart.sh')
+#   subprocess.run([home])
+
 dgroups_key_binder = None
-dgroups_app_rules = []  # type: list
+dgroups_app_rules = []  # type: List
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
-floating_layout = layout.Floating(
-    float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
-        *layout.Floating.default_float_rules,
-        Match(wm_class="confirmreset"),  # gitk
-        Match(wm_class="makebranch"),  # gitk
-        Match(wm_class="maketag"),  # gitk
-        Match(wm_class="ssh-askpass"),  # ssh-askpass
-        Match(title="branchdialog"),  # gitk
-        Match(title="pinentry"),  # GPG key password entry
-    ]
-)
+floating_layout = layout.Floating(float_rules=[
+    # Run the utility of `xprop` to see the wm class and name of an X client.
+    *layout.Floating.default_float_rules,
+    Match(wm_class='confirmreset'),  # gitk
+    Match(wm_class='makebranch'),  # gitk
+    Match(wm_class='maketag'),  # gitk
+    Match(wm_class='ssh-askpass'),  # ssh-askpass
+    Match(title='branchdialog'),  # gitk
+    Match(title='pinentry'),  # GPG key password entry
+    Match(wm_class='Mailspring'), # Mail client
+], fullscreen_border_width = 0, border_width = 0)
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
@@ -216,9 +367,6 @@ reconfigure_screens = True
 # If things like steam games want to auto-minimize themselves when losing
 # focus, should we respect this or not?
 auto_minimize = True
-
-# When using the Wayland backend, this can be used to configure input devices.
-wl_input_rules = None
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
@@ -228,4 +376,5 @@ wl_input_rules = None
 #
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
-wmname = "LG3D" 
+wmname = "Qtile 0.21.0"
+
---
+++ qtile/.config/qtile/scripts/volume.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# You can call this script like this:
+# $./volume.sh up
+# $./volume.sh down
+# $./volume.sh mute
+
+function get_volume {
+    amixer -D pulse get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1
+}
+
+function is_mute {
+    amixer -D pulse get Master | grep '%' | grep -oE '[^ ]+$' | grep off > /dev/null
+}
+
+function send_notification {
+    DIR=`dirname "$0"`
+    volume=`get_volume`
+    # Make the bar with the special character ─ (it's not dash -)
+    # https://en.wikipedia.org/wiki/Box-drawing_character
+#bar=$(seq -s "─" $(($volume/5)) | sed 's/[0-9]//g')
+if [ "$volume" = "0" ]; then
+        icon_name="~/.config/sway/icons/Faba-notifications/notification-audio-volume-muted.svg"
+dunstify -a "Volume $volume" -i "$icon_name" -t 2000 "$bar" -r 555
+    else
+	if [  "$volume" -lt "10" ]; then
+	     icon_name="~/.config/sway/icons/Faba-notifications/notification-audio-volume-low.svg"
+dunstify -a "Volume $volume" -i "$icon_name" -t 2000 "$bar" -r 555
+    else
+        if [ "$volume" -lt "30" ]; then
+            icon_name="~/.config/sway/icons/Faba-notifications/notification-audio-volume-low.svg"
+        else
+            if [ "$volume" -lt "70" ]; then
+                icon_name="~/.config/sway/icons/Faba-notifications/notification-audio-volume-medium.svg"
+            else
+                icon_name="~/.config/sway/icons/Faba-notifications/notification-audio-volume-high.svg"
+            fi
+        fi
+    fi
+fi
+bar=$(seq -s "─" 0 $(($volume/5)) | sed 's/[0-9]//g')
+# Send the notification
+dunstify -a "Volume $volume" -i "$icon_name" -r 555 -u normal "$bar"
+
+}
+
+case $1 in
+    up)
+	# Set the volume on (if it was muted)
+	amixer -D pulse set Master on > /dev/null
+	# Up the volume (+ 5%)
+	amixer -D pulse sset Master 5%+ > /dev/null
+	send_notification
+	;;
+    down)
+	amixer -D pulse set Master on > /dev/null
+	amixer -D pulse sset Master 5%- > /dev/null
+	send_notification
+	;;
+    mute)
+    	# Toggle mute
+	amixer -D pulse set Master 1+ toggle > /dev/null
+	if is_mute ; then
+    DIR=`dirname "$0"`
+    dunstify -i "~/.config/sway/icons/Faba-notifications/notification-audio-volume-muted.svg" -u normal "Mute" -t 2000 -r 555 -a "Volume" 
+	else
+	    send_notification
+	fi
+	;;
+esac
