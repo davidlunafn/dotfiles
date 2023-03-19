5653079f5922299b5d4b00a6eb945b79987a5551
David Luna <davidluna.fn@gmail.com>
2023-03-19 12:17:00 -0500

    configurar neotree

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -5,5 +5,16 @@ return {
         "nvim-lua/plenary.nvim",
         "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim",
-    }
+    },
+    config = function ()
+        require('neo-tree').setup {
+            filesystem = {
+                filtered_items = {
+                    visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+                    hide_dotfiles = false,
+                    hide_gitignored = true,
+                },
+            },
+        }
+    end
 }
